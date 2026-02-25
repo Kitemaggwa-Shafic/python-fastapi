5376bf3a968c76aa57ca952572291dcaa611ec15
Kitemaggwa Shafic <kitemaggwashafic@gmail.com>
2026-02-25 17:19:17 +0400

    adding id to use, importing load & save data classes from storage to use plus shcema classes

--- app/routes/issues.py
+++ app/routes/issues.py
@@ -1,4 +1,9 @@
-from fastapi import APIRouter
+#for no DB, we shall our own id
+import uuid
+from fastapi import APIRouter, HTTPException, status
+from app.schemas import IssueCreate, UpdateIssue, IssueOut
+
+from app.storage import load_data, save_data
 
 #initialising our APIRouter to a var called router, witha prefix of issues 
 #to help us have api with may be version etc
