d72e1aded49da1b1b539f40016d54c10e2aff93e
Kitemaggwa Shafic <kitemaggwashafic@gmail.com>
2026-02-25 17:04:54 +0400

    class create issue from basemodel

--- app/schemas.py
+++ app/schemas.py
@@ -12,3 +12,16 @@ class IssueStatus(str, Enum):
     open = "open"
     in_progress = "in_progress"
     closed = "closed"
+
+class IssuePriority(str, Enum):
+    low = "low"
+    high = "high"
+    critical="critical"
+
+
+#schema for creating an issue
+class IssueCreate(BaseModel):
+    title: str = Field(min_legth=3, max_legth=100) 
+    description: str = Field(min_length=3, max_length=500)
+    priority: IssuePriority = IssuePriority.low 
+  
